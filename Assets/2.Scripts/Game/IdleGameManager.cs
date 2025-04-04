a94bc98be0ed971a6c31e9801d78f528a78715e7
LEE <yttr309@naver.com>
2025-04-04 23:11:01 +0900

    스크롤 추가 UI 변경
    - 추가해야 할 것.. 너무 많아..
    - 광부들 만들어... 나머진 나중에 추가하고
    - 인벤토리 UI, 상점, 강화하기 만들자..

---
+++ Assets/2.Scripts/Game/IdleGameManager.cs
@@ -0,0 +1,44 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdleGameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI CoinText;
+    [SerializeField] private int Coin = 0;
+    private int ClickCoin = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void CoinUpdate()
+    {
+        CoinText.text = "COIN : " + Coin;
+    }
+
+    public void AddClickCoin()
+    {
+        Coin += ClickCoin;
+        CoinUpdate();
+        Debug.Log("눌렀어");
+    }
+
+    public void AddCoin(int Co)
+    {
+        Coin += Co;
+        CoinUpdate();
+    }
+
+    public void SubCoin(int Co)
+    {
+        Coin -= Co;
+        CoinUpdate();
+    }
+}
